c3541a053e5789bc671aac3842d30f1331b3ed9b
Jokūbas Toločka <jokubas.tj@gmail.com>
2024-09-18 14:44:34 +0300

    change type to order in one last file

--- src/components/Confirmation/Confirmation.tsx
+++ src/components/Confirmation/Confirmation.tsx
@@ -5,10 +5,11 @@ import { useState } from "react";
 import ModalContent from "./ModalContent";
 import Container from "@mui/material/Container/Container";
 import Fade from "@mui/material/Fade/Fade";
+import Box from "@mui/material/Box/Box";
 
 const Confirmation = () => {
   const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
-  const { security, amount, type, clearTrade } = useTrade();
+  const { security, amount, order, clearTrade } = useTrade();
 
   const handleClose = () => {
     setIsConfirmationOpen(false);
@@ -17,15 +18,21 @@ const Confirmation = () => {
 
   const confirmTrade = () => {
     // THIS IS WHERE THE REQUEST LOGIC WOULD SIT
-    // await requestTradeConfirmation(url, {security, amount, type}).then(
+    // await requestTradeConfirmation(url, {security, amount, order}).then(
     setIsConfirmationOpen(true);
   };
 
-  const isDisabled = !security || !amount || !type;
+  const isDisabled = !security || !amount || !order;
 
   return (
-    <>
-      <Button variant="contained" disabled={isDisabled} onClick={confirmTrade}>
+    <Box sx={{ mt: 6, width: "100%" }}>
+      <Button
+        variant="contained"
+        disabled={isDisabled}
+        onClick={confirmTrade}
+        fullWidth
+        size="large"
+      >
         Buy {security?.symbol}
       </Button>
       <Modal open={isConfirmationOpen} onClose={handleClose}>
@@ -35,7 +42,7 @@ const Confirmation = () => {
           </Container>
         </Fade>
       </Modal>
-    </>
+    </Box>
   );
 };
 
